56ec148d815faa6994a32358cebf142b1ad2e8ae
Prasheel Soni <prasheelsoni11@gmail.com>
2021-09-21 23:35:51 +0300

    feat: add serviceto create contact

--- src/services/contacts.ts
+++ src/services/contacts.ts
@@ -4,3 +4,14 @@ export const getAllContacts = async(): Promise<Contact[]> => {
     const fetchedContacts = await fetch(`${process.env.REACT_APP_BASE_URL}/contacts`);
     return await fetchedContacts.json();
 }
+
+export const createContact = async(contact: Contact): Promise<string> => {
+    const createdContact = await fetch(`${process.env.REACT_APP_BASE_URL}/contacts`, {
+        method: 'POST',
+        body: JSON.stringify(contact),
+        headers: {
+            authorization:  `Bearer ${process.env.REACT_APP_TOKEN}`
+        }
+    });
+    return await createdContact.json();
+}
